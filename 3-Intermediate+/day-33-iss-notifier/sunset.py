e2ed8ef54c6a3e472fa5a6618a52765431f68e55
Muhammad Ihsan <emhihsan@gmail.com>
2024-07-01 16:29:25 +0700

    add sunset.py

--- 3-Intermediate+/day-33-iss-notifier/sunset.py
+++ 3-Intermediate+/day-33-iss-notifier/sunset.py
@@ -1,9 +1,25 @@
 import requests
-import os
-import time
+from datetime import datetime
 
+MY_LAT = -7.795580
+MY_LONG = 110.369492
 
-MY_LAT = 42.361145
-MY_LONG = -71.057083
-SUNSET_TIME = 0
+parameters = {
+    "lat": MY_LAT,
+    "lng":MY_LONG,
+    "formatted":0,
+    "tzid":"Asia/Jakarta"
+}
 
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
