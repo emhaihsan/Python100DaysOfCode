7ef5952f606048a6716f75d58a41f01015b56b84
Muhammad Ihsan <emhihsan@gmail.com>
2024-07-04 20:38:13 +0700

    finish habit tracker

--- 3-Intermediate+/day-37-habit-tracker/main.py
+++ 3-Intermediate+/day-37-habit-tracker/main.py
@@ -13,25 +13,24 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
 
-# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+today = datetime.datetime.today()
+today = today.strftime("%Y%m%d")
 
-# graph_config = {
-#     "id": "graph1",
-#     "name": "Cycling Graph",
-#     "unit": "Km",
-#     "type": "float",
-#     "color": "ajisai"
-# }
-
-# headers = {
-#     "X-USER-TOKEN": TOKEN
-# }
-
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
+def create_graph(graph_id, graph_name, graph_unit, graph_type, graph_color):
+    headers = {
+        "X-USER-TOKEN": TOKEN
+    }
+    graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+    graph_config = {
+        "id": graph_id,
+        "name": graph_name,
+        "unit": graph_unit,
+        "type": graph_type,
+        "color": graph_color
+    }
+    response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+    print(response.text)
 
 def post_pixel(graph_id, quantity: float, data = None):
     headers = {
@@ -48,4 +47,27 @@ def post_pixel(graph_id, quantity: float, data = None):
     response = requests.post(url=post_pixel_endpoint, json=post_pixel_config, headers=headers)
     print(response.text)
 
+def update_pixel(graph_id, date, quantity):
+    headers = {
+        "X-USER-TOKEN": TOKEN
+    }
+    update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_id}/{date}"
+    update_pixel_config = {
+        "quantity": str(quantity)
+    }
+    response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+    print(response.text)
+
+def delete_pixel(graph_id, date):
+    headers = {
+        "X-USER-TOKEN": TOKEN
+    }
+    delete_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_id}/{date}"
+    response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+    print(response.text)
+
+
+create_graph("graph1", "Cycling", "Km", "float", "sora")
 post_pixel("graph1", 5,5)
+update_pixel("graph1", today, 10)
+delete_pixel("graph1", today)
