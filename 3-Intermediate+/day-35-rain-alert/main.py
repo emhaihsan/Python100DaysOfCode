d5f10fef6941373ad225ff3a7789a0c84fcac723
Muhammad Ihsan <emhihsan@gmail.com>
2024-07-04 16:18:59 +0700

    fix environment variables

--- 3-Intermediate+/day-35-rain-alert/main.py
+++ 3-Intermediate+/day-35-rain-alert/main.py
@@ -1,22 +1,22 @@
 import requests
+import os
 from twilio.rest import Client
 
-ACCOUNT_SID = "YOUR TWILIO ACCOUNT SID"
-TWILIO_AUTH_TOKEN = "YOUR TWILIO AUTH TOKEN"
+ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-TWILIO_WHATSAPP_NUMBER =  "YOUR TWILIO WHATSAPP NUMBER"
-YOUR_TWILIO_VERIFIED_NUMBER = "YOUR TWILIO VERIFIED NUMBER"
-API_KEY = "YOUR OPENWEATHERMAP API KEY"
+TWILIO_WHATSAPP_NUMBER = os.environ.get('TWILIO_WHATSAPP_NUMBER')
+YOUR_TWILIO_VERIFIED_NUMBER = os.environ.get('YOUR_TWILIO_VERIFIED_NUMBER')
+OWM_API_KEY = os.environ.get('OWM_API_KEY')
 MY_LAT = -7.795580 # Your latitude
 MY_LONG = 110.369492 # Your longitude
 
 weather_params = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "appid": API_KEY,
+    "appid": OWM_API_KEY,
     "cnt": 4,
     }
-
 response = requests.get(OWM_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
